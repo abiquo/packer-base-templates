2674eced14c04a599737f99a02b04a772cfec17b
Marc Cirauqui <marc.cirauqui@abiquo.com>
2017-05-10 12:20:35 +0200

    Fix VyOS template

--- scripts/vyos-init.sh
+++ scripts/vyos-init.sh
@@ -8,15 +8,15 @@ sudo sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
 WRAPPER=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
 
 $WRAPPER begin
-$WRAPPER set system package repository vyatta4people url http://packages.vyatta4people.org/debian
-$WRAPPER set system package repository vyatta4people distribution experimental
-$WRAPPER set system package repository vyatta4people components main
+$WRAPPER set system package repository community components 'main'
+$WRAPPER set system package repository community distribution 'current'
+$WRAPPER set system package repository community url 'http://dev.packages.vyos.net/vyos'
+$WRAPPER set system package repository squeeze components 'main contrib non-free'
+$WRAPPER set system package repository squeeze distribution 'squeeze'
+$WRAPPER set system package repository squeeze url 'http://archive.debian.org/debian'
 $WRAPPER commit
 $WRAPPER save
 $WRAPPER end
 
-sudo sed -i /vyos/d /etc/apt/sources.list
-sudo gpg --keyserver pgpkeys.mit.edu --recv-key AED4B06F473041FA
-sudo gpg -a --export AED4B06F473041FA | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y --force-yes install vyatta-clone-helper
+sudo apt-get -y update
+sudo apt-get -y install open-vm-tools
