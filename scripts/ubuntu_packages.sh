fd21ccd272c60e07b83e21fe59d6042fb0fe3f24
Marc Cirauqui <marc.cirauqui@abiquo.com>
2017-09-19 18:12:20 +0200

    Add net-tools package

--- scripts/ubuntu_packages.sh
+++ scripts/ubuntu_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes update && apt-get -y dist-upgrade
-DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install perl ntp curl vim htop ccze wget git gnupg2 unzip
+DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install perl ntp curl vim htop ccze wget git gnupg2 unzip net-tools
