232e1bca8935b294e957db0b7fd014473777a4c4
Marc Cirauqui <marc.cirauqui@abiquo.com>
2017-10-14 19:00:02 +0200

    Remove chef agent

--- scripts/chef-agent-opensuse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-zypper -n --non-interactive install git
-git clone https://github.com/abiquo/abiquo-chef-agent /tmp/abiquo-chef-agent
-cd /tmp/abiquo-chef-agent
-git checkout suse
-bash install/install-opensuse.sh
-cp bin/abiquo-chef-run /usr/bin/abiquo-chef-run.ruby2.1
