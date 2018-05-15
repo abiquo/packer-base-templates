f50bff56ec9bb556e86aaee35879913eec80395b
Marc Cirauqui <marc.cirauqui@abiquo.com>
2018-05-15 12:08:28 +0200

    Added resolvconf

--- scripts/centos_packages.sh
+++ scripts/centos_packages.sh
@@ -5,7 +5,9 @@ echo "add_drivers+=\"virtio virtio_blk virtio_net virtio_pci hv_vmbus hv_storvsc
 
 yum -y install epel-release
 yum -y update
-yum -y install ntp curl vim htop ccze wget git yum-utils unzip yum-cron kernel-devel bash-completion make gcc gcc-c++ haveged jq sudo resolvconf
+yum -y install ntp curl vim htop ccze wget git yum-utils yum-cron \
+  kernel-devel bash-completion make gcc gcc-c++ haveged jq sudo \
+  resolvconf unzip
 
 if [ -x "/usr/bin/systemctl" ]; then
   systemctl enable haveged
