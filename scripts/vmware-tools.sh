e3fc1f8d687926b0dce38c7c199ce0877c68d96b
Marc Cirauqui <marc.cirauqui@abiquo.com>
2018-06-28 12:12:56 +0200

    OpenVM tools in CentOS

--- scripts/vmware-tools.sh
+++ scripts/vmware-tools.sh
@@ -2,11 +2,13 @@
 
 ISSUE=$(cat /etc/issue)
 
+# Ubuntu / Debian
 if [[ "$ISSUE" == "Ubuntu"* ]] || [[ "$ISSUE" == "Debian"* ]]; then
   apt-get -y install open-vm-tools
   exit $?
 fi
 
+# SuSE
 if [[ "$ISSUE" == *"openSUSE"* ]]; then
   zypper -n --non-interactive install open-vm-tools
   echo "force_drivers+=\"virtio virtio_blk virtio_net virtio_pci vmw_pvscsi vmxnet3 vmw_baloon hv_storsvc hv_vmbus hv_utils hv_ballon hv_netsvc\"" >> /etc/dracut.conf.d/01-dist.conf
@@ -14,7 +16,8 @@ if [[ "$ISSUE" == *"openSUSE"* ]]; then
   exit $?
 fi
 
-if [[ "$ISSUE" == *"CentOS"* ]] || [[ "$ISSUE" == *"Atomic"* ]]; then
+# CentOS
+if [[ -f "/etc/redhat-release" ]]; then
 	yum -y install open-vm-tools
   dracut -f
   exit $?
