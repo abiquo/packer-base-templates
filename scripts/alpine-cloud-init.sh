8747c16c4f519c8e107a1df492c5ff939c62d49f
Marc Cirauqui <marc.cirauqui@abiquo.com>
2018-08-22 10:00:01 +0200

    Alpine CInit fixes

--- scripts/alpine-cloud-init.sh
+++ scripts/alpine-cloud-init.sh
@@ -2,33 +2,12 @@
 sed -i "s,alpine/.*/,alpine/edge/,g" /etc/apk/repositories
 
 apk --update add py-setuptools py-urllib3 py-chardet py-certifi py-idna cloud-init curl
-easy_install-2.7 pip
-pip install six
-
-tmpcfg=$(mktemp -d)
-cp -rp /etc/cloud/* $tmpcfg/
-
-tmpdir=$(mktemp -d)
-
-cd $tmpdir
-curl -L https://launchpad.net/cloud-init/trunk/18.1/+download/cloud-init-18.1.tar.gz -o cloud-init-18.1.tar.gz
-tar xf cloud-init-18.1.tar.gz
-cd cloud-init-18.1
-python setup.py install --init-system=sysvinit_openrc
-chmod 755 /etc/init.d/cloud-init*
-
-rm -rf /etc/cloud/*
-cp -rp $tmpcfg/* /etc/cloud/
 
 cat << ENOENT >> /etc/cloud/cloud.cfg
 datasource_list: [ ConfigDrive ]
 ENOENT
 
 rc-update add cloud-init default
-
-cd
-rm -rf $tmpcfg
-rm -rf $tmpdir
 
 # Make sure CD support is loaded on boot
 echo "iso9660" >> /etc/modules
